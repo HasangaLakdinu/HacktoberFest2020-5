117af2ee6421b3a78800ff3aadf821eb888db6ac
Hasanga Lakdinu <36729333+HasangaLakdinu@users.noreply.github.com>
2020-10-27 15:38:44 +0530

    add java file

---
+++ AddI.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmi;
+
+import java.rmi.Remote;
+public interface AddI extends Remote {
+    public int add(int x,int y)throws Exception;
+}
